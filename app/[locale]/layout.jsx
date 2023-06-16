d1a88845b448477c1d308d87b69f3dcd19c9480f
Sashcotravel <93523518+Sashcotravel@users.noreply.github.com>
2023-06-16 11:35:43 +0300

    update footer || header

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -2,7 +2,6 @@
 
 import './globals.css'
 import {NextIntlClientProvider} from 'next-intl';
-import {notFound} from 'next/navigation';
 import {TheFooter} from "../component/footer";
 import {TheHeader} from "../component/header";
 
@@ -15,23 +14,24 @@ export function generateStaticParams() {
   return [{locale: 'en'}, {locale: 'ua'}, {locale: 'ru'}];
 }
 
-export default async function RootLayout({children, params: { locale }}) {
+export default async function RootLayout(props) {
 
+  // {children, params}
   let messages;
   try {
-    messages = (await import(`../../messages/${locale}.json`)).default;
+    messages = (await import(`../../messages/${props.params.locale}.json`)).default;
   } catch (error) {
-    notFound();
+
   }
 
 
   return (
-    <html lang={locale}>
-    <NextIntlClientProvider locale={locale} messages={messages}>
+    <html lang={props?.params.locale}>
+    <NextIntlClientProvider locale={props?.params.locale} messages={messages}>
       <body>
       <TheHeader />
       <main>
-        {children}
+        {props?.children}
       </main>
       <TheFooter />
       </body>
