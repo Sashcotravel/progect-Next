7b82d2275c1a4fe4f49cf5388192e1fc62654fd7
Sashcotravel <93523518+Sashcotravel@users.noreply.github.com>
2023-06-19 17:12:10 +0300

    update all

--- app/[locale]/equipment/head.jsx
+++ app/[locale]/equipment/head.jsx
@@ -10,8 +10,8 @@ export default function Head(){
             <meta name="description" content={t(`equipPage.metaDesc`)} />
             <meta property="og:title" content={t(`equipPage.metaTitle`)} />
             <meta property="og:description" content={t(`equipPage.metaDesc`)} />
-            <link rel="apple-touch-icon" href="../../logo144.png"/>
-            <meta property="og:image" content="../../logo144.png"/>
+            <link rel="apple-touch-icon" href="https://progect-next-nine.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo144.5d64867a.png&w=256&q=75"/>
+            <meta property="og:image" content="https://progect-next-nine.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo144.5d64867a.png&w=256&q=75"/>
             <meta property="og:url" content={locale === 'en' ? "https://samwash.ua/en/equipment" : locale === 'ru' ? "https://samwash.ua/ru/equipment" : "https://samwash.ua/equipment"} />
             <link rel="canonical" href={locale === 'en' ? "https://samwash.ua/en/equipment" : locale === 'ru' ? "https://samwash.ua/ru/equipment" : "https://samwash.ua/equipment"} /><link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css' />
             {/*<script type="application/ld+json">*/}
