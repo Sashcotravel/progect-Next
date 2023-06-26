78ac1c1601cd532d348833aff327e989e45c762b
Sashcotravel <93523518+Sashcotravel@users.noreply.github.com>
2023-06-26 12:04:47 +0300

    update main meta tag

--- app/[locale]/head.jsx
+++ app/[locale]/head.jsx
@@ -10,24 +10,19 @@ export default function Head(){
             <meta name="description" content={t("mainDesc")} />
             <meta property="og:title" content={t("mainTit")} />
             <meta property="og:description" content={t("mainDesc")} />
-            <link rel="apple-touch-icon" href={require('../../image/svg/logo144.png')} />
-            {/*<link rel="apple-touch-icon" href="https://samwash.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo144.5d64867a.png&w=256&q=75"/>*/}
-            <meta property="og:image" content={require('../../image/svg/logo144.png')} />
-             <meta property="og:url" content={locale === 'en' ? "https://samwash.ua/en/" : locale === 'ru' ? "https://samwash.ua/ru/" : "https://samwash.ua/"} />
-             <link rel="canonical" href={locale === 'en' ? "https://samwash.ua/en/" : locale === 'ru' ? "https://samwash.ua/ru/" : "https://samwash.ua/"} />
-            {/*<script type="application/ld+json">*/}
-            {/*      { `"@context": "https://schema.org",*/}
-            {/*      "@type": ["VideoObject", "LearningResource"],*/}
-            {/*      "name": "${t("porohTitSerch")}",*/}
-            {/*      "description": "${t("porohDescSerch")}",*/}
-            {/*      "learningResourceType": "Concept Overview",*/}
-            {/*      "educationalLevel": "SamWash",*/}
-            {/*      "contentUrl": "${locale === 'ru' ? "https://samwash.ua/ru/porohotyag" : locale === 'en' ? "https://samwash.ua/en/porohotyag" : "https://samwash.ua/porohotyag"}",*/}
-            {/*      "thumbnailUrl": [*/}
-            {/*        "https://www.youtube.com/watch?v=OJv67JG7QRo"*/}
-            {/*      ],*/}
-            {/*      "uploadDate": "2023-03-31T08:00:00+08:00"*/}
-            {/*    `}</script>*/}
+            <link rel="apple-touch-icon" href={'../logo144.png'} />
+            {/*/!*<link rel="apple-touch-icon" href="https://samwash.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo144.5d64867a.png&w=256&q=75"/>*!/*/}
+            <meta property="og:image" content={'../logo144.png'} />
+            <meta property="og:url" content={locale === 'en' ? "https://samwash.ua/en/" : locale === 'ru' ? "https://samwash.ua/ru/" : "https://samwash.ua/"} />
+            <link rel="canonical" href={locale === 'en' ? "https://samwash.ua/en/" : locale === 'ru' ? "https://samwash.ua/ru/" : "https://samwash.ua/"} />
+           {/* <script type="application/ld+json">*/}
+           {/*     {`*/}
+           {/*         "@context": "https://schema.org",*/}
+           {/*         "@type": "Organization",*/}
+           {/*         "url": "https://samwash.ua/",*/}
+           {/*         "logo": "%PUBLIC_URL%/logo144.png"*/}
+           {/*     `}*/}
+           {/*</script>*/}
         </React.Fragment>
     )
 }
