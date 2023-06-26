017aab2ac8e97254cdfe78168afab6739af67a38
Sashcotravel <93523518+Sashcotravel@users.noreply.github.com>
2023-06-26 12:15:20 +0300

    update main page

--- app/[locale]/head.jsx
+++ app/[locale]/head.jsx
@@ -10,9 +10,9 @@ export default function Head(){
             <meta name="description" content={t("mainDesc")} />
             <meta property="og:title" content={t("mainTit")} />
             <meta property="og:description" content={t("mainDesc")} />
-            <link rel="apple-touch-icon" href={'../logo144.png'} />
+            <link rel="apple-touch-icon" href='https://samwash.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo144.5d64867a.png&w=256&q=75' />
             {/*/!*<link rel="apple-touch-icon" href="https://samwash.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo144.5d64867a.png&w=256&q=75"/>*!/*/}
-            <meta property="og:image" content={'../logo144.png'} />
+            <meta property="og:image" content='https://progect-next-sashcotravel.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo144.5d64867a.png&w=256&q=75' />
             <meta property="og:url" content={locale === 'en' ? "https://samwash.ua/en/" : locale === 'ru' ? "https://samwash.ua/ru/" : "https://samwash.ua/"} />
             <link rel="canonical" href={locale === 'en' ? "https://samwash.ua/en/" : locale === 'ru' ? "https://samwash.ua/ru/" : "https://samwash.ua/"} />
            {/* <script type="application/ld+json">*/}
