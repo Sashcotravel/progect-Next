999eff2ad002764a0bcaaf9edd8ee5e0e0f9de45
Sashcotravel <93523518+Sashcotravel@users.noreply.github.com>
2023-06-28 18:10:22 +0300

    update blog

--- app/[locale]/blog/loading.jsx
+++ app/[locale]/blog/loading.jsx
@@ -1,5 +1,5 @@
 import s from './blog.module.css'
 
 export default function LoadingPosts(){
-    return <div style={{background: "transparent"}} className={s.loader}>Loading...</div>
+    return <div style={{backgroundColor: "#252f33"}} className={s.loader}>Loading...</div>
 }
