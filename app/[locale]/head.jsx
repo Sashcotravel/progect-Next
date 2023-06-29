cc9043a18a956684a8dfa214e1306b8673fd69d8
Sashcotravel <93523518+Sashcotravel@users.noreply.github.com>
2023-06-29 12:51:50 +0300

    update main script

--- app/[locale]/head.jsx
+++ app/[locale]/head.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import {useLocale, useTranslations} from "next-intl";
 import Script from "next/script";
 
+const schema = {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    "url": "https://samwash.ua/",
+    "logo": "https://samwash.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo144.5d64867a.png&w=256&q=75"
+}
+
 export default function Head(){
     const t = useTranslations();
     const locale = useLocale();
@@ -21,14 +28,7 @@ export default function Head(){
             <link rel="alternate" hrefLang="ru" href="https://samwash.ua/ru/" />
             <link rel="alternate" hrefLang="uk-UA" href="https://samwash.ua/" />
             <link rel="alternate" hrefLang="x-default" href="https://samwash.ua/" />
-            {/*<Script type="application/ld+json" id="application/ld+json">*/}
-                {`
-                    "@context": "https://schema.org",
-                    "@type": "Organization",
-                    "url": "https://samwash.ua/",
-                    "logo": "https://samwash.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo144.5d64867a.png&w=256&q=75"
-                `}
-           {/*</Script>*/}
+            <Script type="application/ld+json" key="structured-data" dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }} />
         </React.Fragment>
     )
 }
