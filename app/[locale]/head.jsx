103e8570ebd1c9d74c0feac3a906a335938fc63c
Sashcotravel <93523518+Sashcotravel@users.noreply.github.com>
2023-06-29 12:35:17 +0300

    update main script

--- app/[locale]/head.jsx
+++ app/[locale]/head.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {useLocale, useTranslations} from "next-intl";
+import Script from "next/script";
 
 export default function Head(){
     const t = useTranslations();
@@ -15,20 +16,19 @@ export default function Head(){
             <link rel="apple-touch-icon" href='https://samwash.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo144.5d64867a.png&w=256&q=75' />
             <meta property="og:image" content='https://progect-next-sashcotravel.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo144.5d64867a.png&w=256&q=75' />
             <meta property="og:url" content={locale === 'en' ? "https://samwash.ua/en/" : locale === 'ru' ? "https://samwash.ua/ru/" : "https://samwash.ua/"} />
-            {/*<link rel="canonical" href={locale === 'en' ? "https://samwash.ua/en/" : locale === 'ru' ? "https://samwash.ua/ru/" : "https://samwash.ua/"} />*/}
-            <link rel="canonical" href={locale === 'en' ? "http://samwash.vercel.app/en/" : locale === 'ru' ? "http://samwash.vercel.app/ru/" : "http://samwash.vercel.app/"} />
-            <link href="http://samwash.vercel.app/en/" rel="alternate" hrefLang="en-UA"/>
-            <link href="http://samwash.vercel.app/ru/" rel="alternate" hrefLang="ru-UA"/>
-            <link href="http://samwash.vercel.app/" rel="alternate" hrefLang="ua"/>
-            <link href="http://samwash.vercel.app/" rel="alternate" hrefLang="x-default"/>
-           {/* <script type="application/ld+json">*/}
-           {/*     {`*/}
-           {/*         "@context": "https://schema.org",*/}
-           {/*         "@type": "Organization",*/}
-           {/*         "url": "https://samwash.ua/",*/}
-           {/*         "logo": "%PUBLIC_URL%/logo144.png"*/}
-           {/*     `}*/}
-           {/*</script>*/}
+            <link rel="canonical" href={locale === 'en' ? "https://samwash.ua/en/" : locale === 'ru' ? "https://samwash.ua/ru/" : "https://samwash.ua/"} />
+            <link rel="alternate" hrefLang="en" href="https://samwash.ua/en/" />
+            <link rel="alternate" hrefLang="ru" href="https://samwash.ua/ru/" />
+            <link rel="alternate" hrefLang="uk-UA" href="https://samwash.ua/" />
+            <link rel="alternate" hrefLang="x-default" href="https://samwash.ua/" />
+            <Script type="application/ld+json">
+                {`
+                    "@context": "https://schema.org",
+                    "@type": "Organization",
+                    "url": "https://samwash.ua/",
+                    "logo": "https://samwash.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo144.5d64867a.png&w=256&q=75"
+                `}
+           </Script>
         </React.Fragment>
     )
 }
